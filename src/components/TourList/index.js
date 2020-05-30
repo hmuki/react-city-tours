f8d9ba3d6f99c629162e58e4d9be0812f6c833da
Muki Hoffmann <Hoffmann@FVFW9ZQGJ1WKs-MacBook-Air.local>
2020-05-31 00:21:07 +0100

    first time using git to deploy on netlify

---
+++ src/components/TourList/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Tour from "../Tour";
+import "./tourlist.scss";
+import { tourData } from "../../tourData.js";
+
+export default class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
+  removeTour = (id) => {
+    const { tours } = this.state;
+    const filteredTours = tours.filter((tour) => tour.id !== id);
+    this.setState({
+      tours: filteredTours,
+    });
+  };
+  render() {
+    // console.log(this.state.tours);
+    const { tours } = this.state;
+    return (
+      <section className="tourlist">
+        {tours.map((tour) => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour}></Tour>
+        ))}
+      </section>
+    );
+  }
+}
